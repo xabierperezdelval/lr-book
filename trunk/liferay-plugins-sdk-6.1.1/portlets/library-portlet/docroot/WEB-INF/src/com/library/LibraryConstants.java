4bd753bb39988adfbbe33f7cde3be839020fcb6a
hasan@mpowerglobal.com <hasan@mpowerglobal.com@d1581b5e-397b-3fa3-08d2-c1164ece2d5d>
2013-01-23 06:11:03 +0000

    Introduced a new constant to replace the update.jsp file name.

--- trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryConstants.java
+++ trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryConstants.java
@@ -3,4 +3,6 @@ package com.library;
 public interface LibraryConstants {
 
 	static final String ACTION_UPDATE_BOOK = "updateBook";
+	
+	static final String PAGE_UPDATE = "/html/library/update.jsp";
 }
