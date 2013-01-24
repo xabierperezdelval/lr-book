3c932cab6c38334fa072415a1376232e4d6fbfb9
hasan@mpowerglobal.com <hasan@mpowerglobal.com@d1581b5e-397b-3fa3-08d2-c1164ece2d5d>
2013-01-24 06:40:47 +0000

    Changes made after generating the service layer and using the persistence API to save data.

--- trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
+++ trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
@@ -1,13 +1,18 @@
 package com.library;
 
 import java.io.IOException;
+import java.util.Date;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 import javax.portlet.PortletException;
 
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
+import com.slayer.model.LMSBook;
+import com.slayer.model.impl.LMSBookImpl;
+import com.slayer.service.LMSBookLocalServiceUtil;
 
 /**
  * Portlet implementation class LibraryPortlet
@@ -21,7 +26,34 @@ public class LibraryPortlet extends MVCPortlet {
 		String bookTitle = ParamUtil.getString(actionRequest, "bookTitle");
 		String author = ParamUtil.getString(actionRequest, "author");
 		
-		System.out.println("Your inputs ==> " + bookTitle + ", " + author);
+		insertBook(bookTitle, author);
+	}
 
+	private void insertBook(String bookTitle, String author) {
+		// 1. Instantiate an empty object of type LMSBookImpl
+		LMSBook lmsBook = new LMSBookImpl();
+		
+		// 2. Generate a unique primary key to be set
+		/*
+		long bookId = 0l;
+		
+		try {
+			bookId = CounterLocalServiceUtil.increment();
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}*/
+		
+		// 3. Set the fields for this object
+		//lmsBook.setBookId(bookId);
+		lmsBook.setBookTitle(bookTitle);
+		lmsBook.setAuthor(author);
+		lmsBook.setCreateDate(new Date());
+		
+		// 4. Call the Service Layer API to persist the object
+		try {
+			lmsBook = LMSBookLocalServiceUtil.addLMSBook(lmsBook);
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
 	}
 }
