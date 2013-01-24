b702775ff377210705871685bf8586de08abc69c
hasan@mpowerglobal.com <hasan@mpowerglobal.com@d1581b5e-397b-3fa3-08d2-c1164ece2d5d>
2013-01-24 12:21:13 +0000

    Redirecting to success.jsp after the book gets added.

--- trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryConstants.java
+++ trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryConstants.java
@@ -5,4 +5,6 @@ public interface LibraryConstants {
 	static final String ACTION_UPDATE_BOOK = "updateBook";
 	
 	static final String PAGE_UPDATE = "/html/library/update.jsp";
+	
+	static final String PAGE_SUCCESS = "/html/library/success.jsp";
 }
