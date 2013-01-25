4d29269d248a8ba00c864a80e303dc3787a5cd5f
hasan <hasan@mpowerglobal.com>
2013-01-25 11:36:09 +0000

    Code changes at the end of chapter 5.

--- liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
+++ liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
@@ -1,7 +1,6 @@
 package com.library;
 
 import java.io.IOException;
-import java.util.Date;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
@@ -10,14 +9,12 @@ import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
 
-import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.PortletURLFactoryUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
 import com.slayer.model.LMSBook;
-import com.slayer.model.impl.LMSBookImpl;
 import com.slayer.service.LMSBookLocalServiceUtil;
 
 /**
@@ -32,7 +29,7 @@ public class LibraryPortlet extends MVCPortlet {
 		String bookTitle = ParamUtil.getString(actionRequest, "bookTitle");
 		String author = ParamUtil.getString(actionRequest, "author");
 		
-		insertBook(bookTitle, author);
+		LMSBookLocalServiceUtil.insertBook(bookTitle, author);
 		
 		// redirect after insert
 		ThemeDisplay themeDisplay = 
@@ -51,33 +48,5 @@ public class LibraryPortlet extends MVCPortlet {
 		
 		successPageURL.setParameter("jspPage", LibraryConstants.PAGE_SUCCESS);
 		actionResponse.sendRedirect(successPageURL.toString());
-	}
-
-	private void insertBook(String bookTitle, String author) {
-		// 1. Instantiate an empty object of type LMSBookImpl
-		LMSBook lmsBook = new LMSBookImpl();
-		
-		// 2. Generate a unique primary key to be set
-		/*
-		long bookId = 0l;
-		
-		try {
-			bookId = CounterLocalServiceUtil.increment();
-		} catch (SystemException e) {
-			e.printStackTrace();
-		}*/
-		
-		// 3. Set the fields for this object
-		//lmsBook.setBookId(bookId);
-		lmsBook.setBookTitle(bookTitle);
-		lmsBook.setAuthor(author);
-		lmsBook.setCreateDate(new Date());
-		
-		// 4. Call the Service Layer API to persist the object
-		try {
-			lmsBook = LMSBookLocalServiceUtil.addLMSBook(lmsBook);
-		} catch (SystemException e) {
-			e.printStackTrace();
-		}
 	}
 }
