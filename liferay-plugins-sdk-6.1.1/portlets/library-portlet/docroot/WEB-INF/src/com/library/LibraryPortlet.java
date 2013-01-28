9d7d446398015a4fc02a13ccccb92ea7d1d6beca
hasan <hasan@mpowerglobal.com>
2013-01-28 10:47:46 +0000

    After implementing the changes for sections 6.2 (dependency Jar's and Tld's) and 6.3 (Adding link to delete book)

--- liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
+++ liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/LibraryPortlet.java
@@ -9,12 +9,13 @@ import javax.portlet.PortletException;
 import javax.portlet.PortletRequest;
 import javax.portlet.PortletURL;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.theme.ThemeDisplay;
 import com.liferay.portlet.PortletURLFactoryUtil;
 import com.liferay.util.bridges.mvc.MVCPortlet;
-import com.slayer.model.LMSBook;
 import com.slayer.service.LMSBookLocalServiceUtil;
 
 /**
@@ -48,5 +49,23 @@ public class LibraryPortlet extends MVCPortlet {
 		
 		successPageURL.setParameter("jspPage", LibraryConstants.PAGE_SUCCESS);
 		actionResponse.sendRedirect(successPageURL.toString());
+	}
+	
+	public void deleteBook(ActionRequest actionRequest,
+			ActionResponse actionResponse) throws IOException, PortletException {
+	
+		long bookId = ParamUtil.getLong(actionRequest, "bookId");
+		
+		if (bookId > 0l) { // valid bookId
+			try {
+				LMSBookLocalServiceUtil.deleteLMSBook(bookId);
+			} catch (PortalException | SystemException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// gracefully redirecting to the default list view
+		String redirectURL = ParamUtil.getString(actionRequest, "redirectURL");
+		actionResponse.sendRedirect(redirectURL);
 	}
 }
