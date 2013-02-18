73d86d1165abab46fe6dc6baa508bffd23ae46fe
hasan@mpowerglobal.com <hasan@mpowerglobal.com@d1581b5e-397b-3fa3-08d2-c1164ece2d5d>
2013-02-18 08:28:07 +0000

    Triggering email from Portlet.

--- trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/job/NotifyDefaultingMembers.java
+++ trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/job/NotifyDefaultingMembers.java
@@ -1,8 +1,25 @@
 package com.library.job;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Enumeration;
+
+import javax.mail.internet.InternetAddress;
+import javax.portlet.PortletPreferences;
+
+import com.liferay.mail.service.MailServiceUtil;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.mail.MailMessage;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.kernel.messaging.MessageListenerException;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.model.User;
+import com.liferay.portal.service.PortalPreferencesLocalServiceUtil;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.slayer.model.LMSBorrowing;
 
 public class NotifyDefaultingMembers implements MessageListener {
 
@@ -11,6 +28,68 @@ public class NotifyDefaultingMembers implements MessageListener {
 			throws MessageListenerException {
 
 		System.out.println("inside receive method....");
+		
+	}
+	
+	private void notifyMember(LMSBorrowing borrowing, 
+			String subjectTemplate, String bodyTemplate) {
+		
+		User member = null;
+		try {
+			member = 
+				UserLocalServiceUtil.fetchUser(borrowing.getMemberId());
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		
+		if (Validator.isNotNull(member)) return;
+		
+		long companyId = member.getCompanyId();
+			
+		InternetAddress fromAddress = getFromAddress(companyId); 
+		MailMessage mailMessage = new MailMessage();
+		mailMessage.setFrom(fromAddress);
+		mailMessage.setSubject(subjectTemplate);
+		mailMessage.setBody(bodyTemplate);
+		
+		String toEmail = member.getEmailAddress();
+		String toName = member.getFullName();		
+		try {
+			mailMessage.setTo(
+				new InternetAddress(toEmail, toName));
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		MailServiceUtil.sendEmail(mailMessage);
+	}
 
+	private InternetAddress getFromAddress(long companyId) {
+		PortletPreferences portletPreferences = null;
+		try {
+			portletPreferences = 
+				PortalPreferencesLocalServiceUtil.getPreferences(
+					companyId, companyId, 1);
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		
+		String key1 = PropsKeys.ADMIN_EMAIL_FROM_ADDRESS;
+		String fromEmail = portletPreferences.getValue(key1, 
+							PropsUtil.get(key1));
+		
+		String key2 = PropsKeys.ADMIN_EMAIL_FROM_NAME;
+		String fromName = portletPreferences.getValue(key2, 
+				PropsUtil.get(key2));
+		
+		InternetAddress fromAddress = new InternetAddress(); 
+		fromAddress.setAddress(fromEmail); 
+		try {
+			fromAddress.setPersonal(fromName);
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		return fromAddress;
 	}
 }
