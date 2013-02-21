1b1691e32bbf92f7eb91be4648d10c1c2af1ad37
hasan <hasan@mpowerglobal.com>
2013-02-21 15:47:21 +0000

    10.7 Non-Standard ways of IPC

--- liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/util/LMSUtil.java
+++ liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/util/LMSUtil.java
@@ -1,10 +1,18 @@
 package com.util;
 
 import java.security.Key;
+import java.util.List;
+import java.util.Locale;
 
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.model.Company;
+import com.liferay.portal.model.Layout;
+import com.liferay.portal.model.LayoutType;
+import com.liferay.portal.model.LayoutTypePortletConstants;
 import com.liferay.portal.service.CompanyLocalServiceUtil;
+import com.liferay.portal.service.LayoutLocalServiceUtil;
 import com.liferay.util.Encryptor;
 import com.liferay.util.EncryptorException;
 
@@ -42,5 +50,50 @@ public class LMSUtil {
 		}
 		
 		return transformedData;
+	}
+	
+	public static long getPlidByName(long groupId, Locale locale, 
+			String name, boolean privateLayout) {
+		long plid = 0l;
+		
+		List<Layout> layouts = null;
+		try {
+			layouts = LayoutLocalServiceUtil.getLayouts(
+						groupId, privateLayout, "portlet");
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		
+		if (Validator.isNotNull(layouts)) {
+			for (Layout layout: layouts) {
+				if (layout.getName(locale).equalsIgnoreCase(name)) {
+					plid = layout.getPlid();
+					break;
+				}
+			}
+		}
+				
+		return plid;
+	}
+	
+	public static long getPlidByFriendlyURL(long groupId, 
+			String friendlyURL, boolean privateLayout) {
+		long plid = 0l;
+		
+		try {
+			Layout layout = 
+				LayoutLocalServiceUtil.getFriendlyURLLayout(
+					groupId, privateLayout, friendlyURL);
+			
+			if (Validator.isNotNull(layout)) {
+				plid = layout.getPlid();
+			}
+		} catch (PortalException e) {
+			e.printStackTrace();
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		
+		return plid;
 	}
 }
