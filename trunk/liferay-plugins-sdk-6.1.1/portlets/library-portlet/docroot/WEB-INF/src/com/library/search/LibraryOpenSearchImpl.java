bfc0047146cbee8ef538ed5228d247071f0fbcfe
hasan@mpowerglobal.com <hasan@mpowerglobal.com@d1581b5e-397b-3fa3-08d2-c1164ece2d5d>
2013-03-14 09:58:47 +0000

    Faceted Search and Open Search, fixing URL issue.

--- trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/search/LibraryOpenSearchImpl.java
+++ trunk/liferay-plugins-sdk-6.1.1/portlets/library-portlet/docroot/WEB-INF/src/com/library/search/LibraryOpenSearchImpl.java
@@ -1,6 +1,13 @@
 package com.library.search;
 
+import javax.portlet.PortletURL;
+
+import com.library.LibraryConstants;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.HitsOpenSearchImpl;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.theme.ThemeDisplay;
 
 public class LibraryOpenSearchImpl extends HitsOpenSearchImpl {
 
@@ -14,5 +21,16 @@ public class LibraryOpenSearchImpl extends HitsOpenSearchImpl {
 
 	public String getTitle(String keywords) {
 		return "Library Open Search for " + keywords;
+	}
+	
+	protected String getURL(ThemeDisplay themeDisplay, long groupId,
+			Document result, PortletURL portletURL) throws Exception {
+		
+		portletURL.setParameter("jspPage", LibraryConstants.PAGE_DETAILS);
+		long bookId = GetterUtil.getLong(
+				result.get(Field.ENTRY_CLASS_PK));
+		portletURL.setParameter("bookId", String.valueOf(bookId));
+	
+		return super.getURL(themeDisplay, groupId, result, portletURL);
 	}
 }
