b9fe2db03fbc4a63dcd315b053e4ddad66a3a7bc
hasan@mpowerglobal.com <hasan@mpowerglobal.com@d1581b5e-397b-3fa3-08d2-c1164ece2d5d>
2013-10-24 09:41:21 +0000

    initial commit

---
+++ trunk/inikah-portlet/docroot/WEB-INF/src/com/inikah/portlet/MainPortlet.java
@@ -0,0 +1,11 @@
+package com.inikah.portlet;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class MainPortlet
+ */
+public class MainPortlet extends MVCPortlet {
+ 
+
+}
