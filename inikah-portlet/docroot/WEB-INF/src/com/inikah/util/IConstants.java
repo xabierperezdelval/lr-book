6dc0fd1fbf048d993f34806ee0b9eca341dd85f0
hasan <hasan@mpowerglobal.com>
2013-10-24 09:48:52 +0000

    initial commit with all necessary files.

---
+++ inikah-portlet/docroot/WEB-INF/src/com/inikah/util/IConstants.java
@@ -0,0 +1,12 @@
+package com.inikah.util;
+
+public interface IConstants {
+	String PROFILE_STATUS_CREATED = "created";
+	String PROFILE_STATUS_DRAFT = "draft";
+	String PROFILE_STATUS_PENDING = "pending";
+	String PROFILE_STATUS_ACTIVE = "active";
+	String PROFILE_STATUS_DELETED = "deleted";
+	
+	String LIST_MARITAL_STATUS = "maritalStatus"; 
+	String LIST_CREATED_FOR = "createdFor";
+}
