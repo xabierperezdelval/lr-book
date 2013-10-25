3afe5f18789316308a9ce2f8664354c9b4b6c452
hasan <hasan@mpowerglobal.com>
2013-10-25 10:57:15 +0000

    new class added for getting the proper text for height and weight attributes of a profile.

---
+++ inikah-portlet/docroot/WEB-INF/src/com/inikah/util/ListUtil.java
@@ -0,0 +1,67 @@
+package com.inikah.util;
+
+import java.text.DecimalFormat;
+
+import com.liferay.portal.kernel.util.StringPool;
+
+public class ListUtil {
+	
+	public static String getHeightText(int heightCms, boolean htmlSpace) {
+		
+		String delimiter = htmlSpace? StringPool.NBSP : StringPool.SPACE;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(heightCms);
+		sb.append(delimiter);
+		sb.append("Cms");
+		sb.append(delimiter);
+		sb.append(StringPool.SLASH);
+		sb.append(delimiter);		
+		
+		double conversion = 0.39370078740157477d;
+		
+		double totalInches = heightCms * conversion;
+		
+		int foot = (int) Math.floor(totalInches / 12); 
+		
+		sb.append(String.format("%02d", foot));
+		sb.append(delimiter);
+		sb.append("Ft");
+		sb.append(delimiter);
+		
+		double inches = totalInches % 12;
+		
+		DecimalFormat decimalFormat = new DecimalFormat("00.00");
+		sb.append(decimalFormat.format(inches));
+		
+		sb.append(delimiter);
+		sb.append("Inch");
+		
+		return sb.toString();
+	}
+	
+	public static String getWeightText(int weightKgs, boolean htmlSpace) {
+		
+		String delimiter = htmlSpace? StringPool.NBSP : StringPool.SPACE;
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(weightKgs);
+		sb.append(delimiter);
+		sb.append("Kgs");
+		sb.append(delimiter);
+		sb.append(StringPool.SLASH);
+		sb.append(delimiter);			
+		
+		double conversion = 2.20462d;
+		
+		double pounds = weightKgs * conversion;
+		
+		DecimalFormat decimalFormat = new DecimalFormat("000.00");
+		sb.append(decimalFormat.format(pounds));
+		
+		sb.append(delimiter);
+		sb.append("Lbs");
+		
+		return sb.toString();
+	}
+}
