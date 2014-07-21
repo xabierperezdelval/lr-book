56dc176657bf6f4141512be519d8570579ad5e2f
atif.ali <atif.ali@mpowerglobal.com>
2014-07-21 11:59:51 +0000

    Modified: 1) The menu self creation 2) The menu setup

--- fingence-portlet/docroot/WEB-INF/src/com/fingence/hook/CreateReportTypes.java
+++ fingence-portlet/docroot/WEB-INF/src/com/fingence/hook/CreateReportTypes.java
@@ -1,38 +1,120 @@
 package com.fingence.hook;
 
+import java.util.List;
+
+import com.fingence.slayer.service.impl.AssetHelper;
 import com.liferay.portal.kernel.events.SimpleAction;
 import com.liferay.portal.kernel.events.ActionException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.service.ServiceContext;
+import com.liferay.portal.service.UserLocalServiceUtil;
+import com.liferay.portal.util.PortalUtil;
+import com.liferay.portlet.asset.model.AssetCategory;
+import com.liferay.portlet.asset.model.AssetVocabulary;
+import com.liferay.portlet.asset.service.AssetCategoryLocalServiceUtil;
+import com.liferay.portlet.asset.service.AssetVocabularyLocalServiceUtil;
 import com.liferay.util.portlet.PortletProps;
 
 public class CreateReportTypes extends SimpleAction {
-	/* (non-Java-doc)
-	 * @see com.liferay.portal.kernel.events.SimpleAction#SimpleAction()
-	 */
 	public CreateReportTypes() {
 		super();
 	}
 
-	/* (non-Java-doc)
-	 * @see com.liferay.portal.kernel.events.SimpleAction#run(String[] arg0)
-	 */
 	public void run(String[] arg0) throws ActionException {
-		// TODO Auto-generated method stub
+		ServiceContext serviceContext = new ServiceContext();
+		serviceContext.setScopeGroupId(AssetHelper.getGuestGroupId());
+		long companyId = PortalUtil.getDefaultCompanyId(); // Long.parseLong(arg0[0])
+		long defaultUserId = 0l;
+		try {
+			defaultUserId = UserLocalServiceUtil.getDefaultUserId(companyId);
+		} catch (PortalException | SystemException e1) {
+			e1.printStackTrace();
+		}
 		
-		// create vocabulary
+		long vocabularyId = 0l;
 		String vocabularyName = PortletProps.get("report.types.vocabulary.name");
 	
-		// create vocabulary if it does NOT exist
+		vocabularyId = checkTreeVocabulary(vocabularyName);
 		
-		//============
-		// create parent and children, recursively
+		if (vocabularyId == 0l) {
+			vocabularyId = createTreeVocabulary(vocabularyName, defaultUserId, serviceContext);
+		}
 		
-		String[] mainReports = PortletProps.getArray("report.types.parent.categories");
-		
-		for (int i=0; i<mainReports.length; i++) {
-			// create the parent if it does NOT exist
+		String[] parentReportNames = PortletProps.getArray("report.types.parent.categories");
+		for (int i = 0; i < parentReportNames.length; i++) {
+			long treeItemId = 0l;
+			treeItemId = checkTreeItem(vocabularyId, parentReportNames[i]);
 			
-			// for each parent create it's children by reading from the properties file
+			if (treeItemId == 0l) {
+				treeItemId = createTreeItem(vocabularyId, 0, parentReportNames[i], defaultUserId, serviceContext);
+			}
+			
+			String childReportPath = "report.types.children." + parentReportNames[i].toLowerCase().trim().replace(" ", ".");
+			String[] childReportNames = PortletProps.getArray(childReportPath);
+			
+			for (int j = 0; j < childReportNames.length; j++) {
+				long treeChildItemId = 0l;
+				treeChildItemId = checkTreeItem(vocabularyId, childReportNames[j]);
+				
+				if (treeChildItemId == 0l) {
+					treeChildItemId = createTreeItem(vocabularyId, treeItemId, childReportNames[j], defaultUserId, serviceContext);
+				}
+			}
 		}
 	}
 
+	private long checkTreeVocabulary(String vocabularyName) {
+		AssetVocabulary assetVocabulary = null;
+		try {
+			assetVocabulary = AssetVocabularyLocalServiceUtil.getGroupVocabulary(AssetHelper.getGuestGroupId(), vocabularyName);
+		} catch (PortalException | SystemException e) {
+			e.printStackTrace();
+		}
+		return assetVocabulary.getVocabularyId();
+	}
+
+	private long createTreeVocabulary(String vocabularyName, long defaultUserId, ServiceContext serviceContext) {
+		AssetVocabulary vocabulary = null;
+		try {
+			vocabulary = AssetVocabularyLocalServiceUtil.addVocabulary(defaultUserId, vocabularyName, serviceContext);
+		} catch (SystemException | PortalException e) {
+			e.printStackTrace();
+		}
+		return vocabulary.getVocabularyId();
+	}
+	
+	private long checkTreeItem(long vocabularyId, String itemName) {
+		long assetCategoryId = 0l;
+		List<AssetCategory> assetCategories = null;
+		try {
+			assetCategories = AssetCategoryLocalServiceUtil.getVocabularyCategories(vocabularyId, 0, AssetCategoryLocalServiceUtil.getAssetCategoriesCount(), null);
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		
+		for(AssetCategory assetCategory:assetCategories) {
+			if(assetCategory.getName().equalsIgnoreCase(itemName)) {
+				assetCategoryId = assetCategory.getCategoryId();
+			}
+		}
+		return assetCategoryId;
+	}
+	
+	private long createTreeItem(long vocabularyId, long parentItemId, String itemName, long defaultUserId, ServiceContext serviceContext) {
+		AssetCategory assetCategory = null;
+		try {
+			assetCategory = AssetCategoryLocalServiceUtil.addCategory(defaultUserId, itemName, vocabularyId, serviceContext);
+		} catch (SystemException | PortalException e) {
+			e.printStackTrace();
+		}
+		assetCategory.setParentCategoryId(parentItemId);
+		try {
+			AssetCategoryLocalServiceUtil.updateAssetCategory(assetCategory);
+		} catch (SystemException e) {
+			e.printStackTrace();
+		}
+		return assetCategory.getCategoryId();
+	}
+	
 }
--- fingence-portlet/docroot/html/report/menu.jsp
+++ fingence-portlet/docroot/html/report/menu.jsp
@@ -4,6 +4,9 @@
 <%@ include file="/html/report/init.jsp" %>
 
 <ul class="left-nav">
+	<c:if test="<%= (userType == IConstants.USER_TYPE_WEALTH_ADVISOR || userType == IConstants.USER_TYPE_WEALTH_ADVISOR) %>">
+		<li class="<%= (layoutName.equalsIgnoreCase(IConstants.PAGE_REPORTS_HOME))? IConstants.SELECTED : StringPool.BLANK %>" id="li_<%= IConstants.PAGE_REPORTS_HOME %>"><a href="javascript:void(0);"  onClick="javascript:triggerRequest('<%= IConstants.ADD_USER %>', '<%= IConstants.PAGE_REPORTS_HOME %>');"><%= TextFormatter.format(IConstants.PAGE_REPORTS_HOME, TextFormatter.J) %></a>
+	</c:if>
 	<%  
 		for (Entry<Long, String> item : ReportConfigServiceUtil.getMenuItems(0).entrySet()) {
 			%><li class="<%= (layoutName.equalsIgnoreCase(item.getValue()))? IConstants.SELECTED : StringPool.BLANK %>" id="li_<%= item.getKey() %>"><a href="javascript:void(0);" onClick="javascript:triggerRequest('<%= item.getKey() %>', '<%= item.getValue() %>');"><%= TextFormatter.format(item.getValue(), TextFormatter.J) %></a><%
@@ -12,7 +15,6 @@
 	<c:if test="<%= (userType == IConstants.USER_TYPE_WEALTH_ADVISOR || userType == IConstants.USER_TYPE_WEALTH_ADVISOR) %>">
 		<li class="<%= (layoutName.equalsIgnoreCase(IConstants.ADD_PORTFOLIO))? IConstants.SELECTED : StringPool.BLANK %>" id="li_<%= IConstants.ADD_PORTFOLIO %>"><a href="javascript:void(0);"  onClick="javascript:triggerRequest('<%= IConstants.ADD_USER %>', '<%= IConstants.ADD_PORTFOLIO %>');"><%= TextFormatter.format(IConstants.ADD_PORTFOLIO, TextFormatter.J) %></a>
 	</c:if>
-	
 	<c:if test="<%= (userType == IConstants.USER_TYPE_WEALTH_ADVISOR) %>">
 		<li class="<%= (layoutName.equalsIgnoreCase(IConstants.ADD_USER))? IConstants.SELECTED : StringPool.BLANK %>" id="li_<%= IConstants.ADD_USER %>"><a href="javascript:void(0);" onClick="javascript:triggerRequest('<%= IConstants.ADD_USER %>', '<%= IConstants.ADD_USER %>');"><%= TextFormatter.format(IConstants.ADD_USER, TextFormatter.J) %></a>
 	</c:if>
